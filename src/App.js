7a544b920ec0a6ce4cf075948b37bc45304aa330
sigood <sigood@umich.edu>
2022-10-01 15:26:22 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Particle from "./components/Particle";
+import Pdf from "./documents/resume.pdf"
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particle />
+      <div className="container">
+        <div className="mt-5 pt-5">
+          <h1 className="mt-5 font-link" style={{ fontSize: "150px", textAlign: "center" }}>
+            Hey, I'm Spencer Goodwin
+          </h1>
+          <div className="text-center" style={{ fontSize: "90px" }}>
+            <a href="https://github.com/Spencerg00dwin">
+              <i className="bi bi-github px-5 icon"></i>
+            </a>
+            <a href={Pdf}>
+              <i className="bi bi-file-earmark-person px-5 icon"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/spencer-goodwin/">
+              <i className="bi bi-linkedin px-5 icon"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
