f9f979b20c4522a6654d84a2a5b5397f41253bb6
Spencerg00dwin <97697401+Spencerg00dwin@users.noreply.github.com>
2023-02-04 16:17:17 -0500

    working on updates to personal page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Particle from "./components/Particle";
-import Pdf from "./documents/resume.pdf"
+import Pdf from "./documents/spencer-resume.pdf"
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -8,24 +8,25 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 export default function App() {
   return (
     <>
-      <Particle />
-      <div className="container">
-        <div className="mt-5 pt-5">
-          <h1 className="mt-5 font-link" style={{ fontSize: "150px", textAlign: "center"}}>
+      {/* <Particle /> */}
+      {/* <div className="container bg-white rounded" style={{ height: "750px"}}> */}
+      <div>
+          <h1 className="mt-5 font-link" style={{ fontSize: "50px"}}>
             Hey, I'm Spencer Goodwin.
           </h1>
           <div className="text-center" style={{ fontSize: "90px" }}>
             <a href="https://github.com/Spencerg00dwin">
               <i className="bi bi-github px-5 icon"></i>
             </a>
-            <a href={Pdf}>
-              <i className="bi bi-file-earmark-person px-5 icon"></i>
-            </a>
+            {/* <a href={Pdf}> */}
+              {/* <embed src={Pdf}>
+                <i className="bi bi-file-earmark-person px-5 icon"></i>
+              </embed> */}
+            {/* </a> */}
             <a href="https://www.linkedin.com/in/spencer-goodwin/">
               <i className="bi bi-linkedin px-5 icon"></i>
             </a>
           </div>
-        </div>
       </div>
     </>
   );
